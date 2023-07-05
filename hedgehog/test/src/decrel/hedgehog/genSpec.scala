381a5ffd990d0276063726b30a8ef7cd3e8fe4a8
Haemin Yoo <haemin@zzz.pe.kr>
2023-07-06 01:12:25 +0900

    --wip-- [skip ci]

---
+++ hedgehog/test/src/decrel/hedgehog/genSpec.scala
@@ -0,0 +1,147 @@
+/*
+ * Copyright (c) 2022 Haemin Yoo
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package decrel.hedgehog
+
+import decrel.*
+import decrel.hedgehog.gen.*
+import _root_.hedgehog.*
+import zio.test.Assertion.*
+import zio.test.{ Gen as _, * }
+
+object genSpec extends ZIOSpecDefault {
+
+  // Relation descriptions
+  case class Rental(id: Rental.Id, bookId: Book.Id, userId: User.Id)
+  object Rental {
+    case class Id(value: String)
+
+    case object self  extends Relation.Self[Rental]
+    case object fetch extends Relation.Single[Rental.Id, Rental]
+    case object book  extends Relation.Single[Rental, Book]
+    case object user  extends Relation.Single[Rental, User]
+  }
+  case class Book(id: Book.Id, currentRental: Option[Rental.Id])
+  object Book {
+    case class Id(value: String)
+
+    case object self          extends Relation.Self[Book]
+    case object fetch         extends Relation.Single[Book.Id, Book]
+    case object currentRental extends Relation.Optional[Book, Rental]
+  }
+  case class User(id: User.Id, currentRentals: List[Rental.Id])
+  object User {
+    case class Id(value: String)
+
+    case object self           extends Relation.Self[User]
+    case object fetch          extends Relation.Single[User.Id, User]
+    case object currentRentals extends Relation.Many[User, List, Rental]
+  }
+
+  // Basic Generators
+  object gen {
+    private val genString = Gen.string(Gen.char('a', 'z'), Range.linear(0, 10))
+
+    val rentalId: Gen[Rental.Id] = genString.map(Rental.Id)
+    val bookId: Gen[Book.Id]     = genString.map(Book.Id)
+    val userId: Gen[User.Id]     = genString.map(User.Id)
+  }
+
+  // Relation Implementations
+  // Note: not all relations are implemented; we need only implement the ones we use
+
+  implicit val rentalFetch: Proof.Single[
+    Rental.fetch.type & Relation.Single[Rental.Id, Rental],
+    Rental.Id,
+    Rental
+  ] = Gen.relationSingle(Rental.fetch) { id =>
+    for {
+      bookId <- gen.bookId
+      userId <- gen.userId
+    } yield Gen.constant(Rental(id, bookId, userId))
+  }
+
+  implicit val rentalBook: Proof.Single[
+    Rental.book.type & Relation.Single[Rental, Book],
+    Rental,
+    Book
+  ] = Gen.relationSingle(Rental.book) { rental =>
+    Gen.constant(Book(rental.bookId, Some(rental.id)))
+  }
+
+  implicit val rentalUser: Proof.Single[
+    Rental.user.type & Relation.Single[Rental, User],
+    Rental,
+    User
+  ] = Gen.relationSingle(Rental.user) { rental =>
+    Gen.constant(User(rental.userId, List(rental.id)))
+  }
+
+  implicit val userCurrentRentals: Proof.Many[
+    User.currentRentals.type & Relation.Many[User, List, Rental],
+    User,
+    List,
+    Rental
+  ] = Gen.relationMany(User.currentRentals) { user =>
+    Gen
+      // Use `expand` even when implementing other relations
+      .list(gen.rentalId.expand(Rental.fetch), Range.linear(0, 10))
+      // Make it consistent
+      .map(_.map(_.copy(userId = user.id)))
+  }
+
+  override def spec: Spec[Environment, Any] =
+    suite("proof - zio.test.Gen")(
+      test("Simple relation") {
+        val relation = Rental.fetch
+
+        val staticRentalId = Rental.Id("foo")
+        val rentalIdGen    = Gen.constant(staticRentalId)
+
+        check(rentalIdGen.expand(relation)) { (rental: Rental) =>
+          assert(rental.id)(equalTo(staticRentalId))
+        }
+      },
+      test("Composing with &") {
+        val relation = Rental.fetch & Rental.fetch
+
+        check(gen.rentalId.expand(relation)) { case (rental1: Rental, rental2: Rental) =>
+          assert(rental1.id)(equalTo(rental2.id))
+          assert(rental1)(not(equalTo(rental2)))
+        }
+      },
+      test("Composing with >>:") {
+        val relation = Rental.fetch >>: Rental.book
+
+        val staticRentalId = Rental.Id("foo")
+        val rentalIdGen    = Gen.constant(staticRentalId)
+
+        check(rentalIdGen.expand(relation)) { (book: Book) =>
+          assert(book.currentRental)(isSome(equalTo(staticRentalId)))
+        }
+      },
+      test("Composing with <>:") {
+        val relation = Rental.fetch <>: Rental.book
+
+        val staticRentalId = Rental.Id("foo")
+        val rentalIdGen    = Gen.constant(staticRentalId)
+
+        check(rentalIdGen.expand(relation)) { case (rental, book) =>
+          assert(rental.id)(equalTo(staticRentalId))
+          assert(book.currentRental)(isSome(equalTo(staticRentalId)))
+        }
+      },
+      test("Complex relations") {
+        val relation = Rental.fetch >>: Rental.user >>: (User.self & User.currentRentals)
+
+        check(gen.rentalId.expand(relation)) { case (user, rentals) =>
+          assert(rentals.map(_.userId))(forall(equalTo(user.id)))
+        }
+      }
+    )
+}
