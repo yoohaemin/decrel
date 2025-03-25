f66a7bd6698898d8dacc312b2d28c25a0999a337
Haemin Yoo <haemin@zzz.pe.kr>
2025-03-26 01:31:15 +0900

    Add tests for many relations and cache operations in zqueryProofSpec

--- zquery/src/test/scala/decrel/reify/zqueryProofSpec.scala
+++ zquery/src/test/scala/decrel/reify/zqueryProofSpec.scala
@@ -248,199 +248,214 @@ object zqueryProofSpec extends ZIOSpecDefault {
             assertZIO(result)(equalTo(Chunk(rental1))) &&
             assertZIO(proofs.calls.get)(equalTo(Calls(User.currentRentals -> Chunk(user1))))
           }
+        },
+        test("Many relation - multiple results") {
+          proofs.flatMap { proofs =>
+            import proofs.*
+            val relation = User.currentRentals
+            val result   = relation.startingFrom(user2)
+
+            assertZIO(result)(equalTo(Chunk(rental2, rental3))) &&
+            assertZIO(proofs.calls.get)(equalTo(Calls(User.currentRentals -> Chunk(user2))))
+          }
+        },
+        test("Many relation - empty result") {
+          proofs.flatMap { proofs =>
+            import proofs.*
+            val relation = User.currentRentals
+            val result   = relation.startingFrom(user3)
+
+            assertZIO(result)(equalTo(Chunk.empty)) &&
+            assertZIO(proofs.calls.get)(equalTo(Calls(User.currentRentals -> Chunk(user3))))
+          }
+        },
+        test("Many relation with batch queries") {
+          proofs.flatMap { proofs =>
+            import proofs.*
+            val relation = User.currentRentals
+            val result   = relation.startingFrom(Chunk(user1, user2, user3))
+
+            assertZIO(result)(
+              equalTo(Chunk(Chunk(rental1), Chunk(rental2, rental3), Chunk.empty))
+            ) &&
+            assertZIO(proofs.calls.get)(
+              equalTo(Calls(User.currentRentals -> Chunk(user1, user2, user3)))
+            )
+          }
+        },
+        test("Many relation with cache") {
+          proofs.flatMap { proofs =>
+            import proofs.*
+
+            val cache = Cache.empty
+              .add(User.currentRentals, user2, Chunk(rental2, rental3))
+
+            val relation = User.currentRentals
+            val result   = relation.startingFrom(user2, cache)
+
+            assertZIO(result)(equalTo(Chunk(rental2, rental3))) &&
+            assertZIO(proofs.calls.get)(equalTo(Calls()))
+          }
         }
       ),
-      suite("Composed relations")(
-        suite("&")(
-          test("simple")(
-            proofs.flatMap { proofs =>
-              import proofs.*
-              val relation = Rental.self & Rental.book & Rental.user
-              val result   = relation.startingFrom(rental1)
+      suite("Cache operations")(
+        test("Single relation with cache") {
+          proofs.flatMap { proofs =>
+            import proofs.*
 
-              assertZIO(result)(equalTo((rental1, book1, user1))) &&
-              assertZIO(proofs.calls.get.map(_.value))(
-                hasSameElements(
-                  Calls(
-                    Book.fetch -> Chunk(book1.id),
-                    User.fetch -> Chunk(user1.id)
-                  ).value
-                )
-              )
-            }
-          ),
-          test("cached")(
-            proofs.flatMap { proofs =>
-              import proofs.*
+            val cache = Cache.empty
+              .add(Book.fetch, book1.id, book1)
 
-              val cache = Cache.empty
-                // Notice that the only implemented proof matters,
-                // not the proof that is used
-                // .add(Rental.book, rental1, book1)
-                .add(Book.fetch, book1.id, book1)
+            val relation = Book.fetch
+            val result   = relation.startingFrom(book1.id, cache)
 
-              val relation = Rental.self & Rental.book
-              val result   = relation.startingFrom(rental1, cache)
-
-              assertZIO(result)(equalTo((rental1, book1))) &&
-              assertZIO(proofs.calls.get)(equalTo(Calls()))
-            }
-          )
-        ),
-        suite(">>:")(
-          test("simple") {
-            proofs.flatMap { proofs =>
-              import proofs.*
-              val relation = Book.currentRental >>: Rental.book
-              val result   = relation.startingFrom(book1)
-
-              assertZIO(result)(isSome(equalTo(book1))) &&
-              assertZIO(proofs.calls.get)(
-                equalTo(
-                  Calls(
-                    Book.currentRental -> Chunk(book1),
-                    Book.fetch         -> Chunk(rental1.bookId)
-                  )
-                )
-              )
-            }
-          },
-          test("cached") {
-            proofs.flatMap { proofs =>
-              import proofs.*
-
-              val cache = Cache.empty
-                .add(Book.fetch, book1.id, book1)
-
-              val relation = Book.currentRental >>: Rental.book
-              val result   = relation.startingFrom(book1, cache)
-
-              assertZIO(result)(isSome(equalTo(book1))) &&
-              assertZIO(proofs.calls.get)(
-                equalTo(
-                  Calls(
-                    Book.currentRental -> Chunk(book1)
-                  )
-                )
-              )
-            }
+            assertZIO(result)(equalTo(book1)) &&
+            assertZIO(proofs.calls.get)(equalTo(Calls()))
           }
-        ),
-        suite("<>:")(
-          test("simple") {
-            proofs.flatMap { proofs =>
-              import proofs.*
-              val relation = Book.currentRental <>: Rental.book
-              val result   = relation.startingFrom(book1)
+        },
+        test("Optional relation with cache - existing") {
+          proofs.flatMap { proofs =>
+            import proofs.*
 
-              assertZIO(result)(isSome(equalTo((rental1, book1)))) &&
-              assertZIO(proofs.calls.get)(
-                equalTo(
-                  Calls(
-                    Book.currentRental -> Chunk(book1),
-                    Book.fetch         -> Chunk(rental1.bookId)
-                  )
-                )
-              )
-            }
-          },
-          test("cached") {
-            proofs.flatMap { proofs =>
-              import proofs.*
+            val cache = Cache.empty
+              .add(Book.currentRental, book1, Some(rental1))
 
-              val cache = Cache.empty
-                .add(Book.fetch, book1.id, book1)
+            val relation = Book.currentRental
+            val result   = relation.startingFrom(book1, cache)
 
-              val relation = Book.currentRental <>: Rental.book
-              val result   = relation.startingFrom(book1, cache)
-
-              assertZIO(result)(isSome(equalTo((rental1, book1)))) &&
-              assertZIO(proofs.calls.get)(
-                equalTo(
-                  Calls(
-                    Book.currentRental -> Chunk(book1)
-                  )
-                )
-              )
-            }
+            assertZIO(result)(isSome(equalTo(rental1))) &&
+            assertZIO(proofs.calls.get)(equalTo(Calls()))
           }
-        ),
-        suite("Optimizations")(
-          suite("deduplication")(
-            test("&")(
-              proofs.flatMap { proofs =>
-                import proofs.*
+        },
+        test("Optional relation with cache - non-existing") {
+          proofs.flatMap { proofs =>
+            import proofs.*
 
-                val relation = Rental.book & Rental.book
-                val result   = relation.startingFrom(rental1)
+            val cache = Cache.empty
+              .add(Book.currentRental, book4, None)
 
-                assertZIO(result)(equalTo((book1, book1))) &&
-                assertZIO(proofs.calls.get)(
-                  equalTo(
-                    // batching and deduplication
-                    Calls(
-                      Book.fetch -> Chunk(book1.id)
-                    )
-                  )
+            val relation = Book.currentRental
+            val result   = relation.startingFrom(book4, cache)
+
+            assertZIO(result)(isNone) &&
+            assertZIO(proofs.calls.get)(equalTo(Calls()))
+          }
+        },
+        test("Multiple cache entries") {
+          proofs.flatMap { proofs =>
+            import proofs.*
+
+            val cache = Cache.empty
+              .add(Book.fetch, book1.id, book1)
+              .add(User.fetch, user1.id, user1)
+              .add(Rental.fetch, rental1.id, rental1)
+
+            val bookRelation   = Book.fetch
+            val userRelation   = User.fetch
+            val rentalRelation = Rental.fetch
+
+            val result = for {
+              book   <- bookRelation.startingFrom(book1.id, cache)
+              user   <- userRelation.startingFrom(user1.id, cache)
+              rental <- rentalRelation.startingFrom(rental1.id, cache)
+            } yield (book, user, rental)
+
+            assertZIO(result)(equalTo((book1, user1, rental1))) &&
+            assertZIO(proofs.calls.get)(equalTo(Calls()))
+          }
+        }
+      ),
+      suite("Composed relations with cache")(
+        test("startingFrom with cache") {
+          proofs.flatMap { proofs =>
+            import proofs.*
+
+            val cache = Cache.empty
+              .add(Book.fetch, book1.id, book1)
+              .add(Book.fetch, book2.id, book2)
+
+            val relation = Book.fetch
+            val result   = relation.startingFrom(List(book1.id, book2.id, book3.id), cache)
+
+            assertZIO(result)(equalTo(List(book1, book2, book3))) &&
+            assertZIO(proofs.calls.get)(equalTo(Calls(Book.fetch -> Chunk(book3.id))))
+          }
+        },
+        test(">>: composition with cache") {
+          proofs.flatMap { proofs =>
+            import proofs.*
+
+            val cache = Cache.empty
+              .add(Book.currentRental, book1, Some(rental1))
+              .add(User.fetch, rental1.userId, user1)
+
+            val relation = Book.currentRental >>: Rental.user
+            val result   = relation.startingFrom(book1, cache)
+
+            assertZIO(result)(isSome(equalTo(user1))) &&
+            assertZIO(proofs.calls.get)(equalTo(Calls()))
+          }
+        },
+        test("complex composition with partial cache") {
+          proofs.flatMap { proofs =>
+            import proofs.*
+
+            val cache = Cache.empty
+              .add(User.fetch, user2.id, user2)
+              // Note: We don't cache User.currentRentals, so that will still be called
+              .add(Book.fetch, book2.id, book2)
+              .add(Book.fetch, book3.id, book3)
+
+            val relation = User.fetch >>: User.currentRentals >>: Rental.book
+            val result   = relation.startingFrom(user2.id, cache)
+
+            assertZIO(result)(equalTo(Chunk(book2, book3))) &&
+            assertZIO(proofs.calls.get)(
+              equalTo(
+                Calls(
+                  User.currentRentals -> Chunk(user2)
                 )
-              }
-            ),
-            test(">>:")(
-              proofs.flatMap { proofs =>
-                import proofs.*
-
-                val relation =
-                  Rental.book >>: Book.currentRental >>: Rental.book >>: Book.currentRental
-                val result = relation.startingFrom(rental1)
-
-                assertZIO(result)(isSome(isSome(equalTo(rental1)))) &&
-                assertZIO(proofs.calls.get)(
-                  equalTo(
-                    Calls(
-                      Book.fetch         -> Chunk(book1.id),
-                      Book.currentRental -> Chunk(book1)
-                    )
-                  )
-                )
-              }
+              )
             )
-          ),
-          test("batching")(
-            proofs.flatMap { proofs =>
-              import proofs.*
+          }
+        },
+        test("many relation composition with cache") {
+          proofs.flatMap { proofs =>
+            import proofs.*
 
-              val relation = Rental.user >>: User.currentRentals >>: Rental.book
-              val result   = relation.startingFrom(rental2)
+            val cache = Cache.empty
+              .add(User.currentRentals, user2, Chunk(rental2, rental3))
+              .add(Book.fetch, book2.id, book2)
+            // Note: book3.id isn't cached
 
-              assertZIO(result)(equalTo(Chunk(book2, book3))) &&
-              assertZIO(proofs.calls.get)(
-                equalTo(
-                  Calls(
-                    User.fetch          -> Chunk(rental2.userId),
-                    User.currentRentals -> Chunk(user2),
-                    // The following two calls have been batched, avoiding N+1 problem
-                    Book.fetch -> Chunk(rental2.bookId, rental3.bookId)
-                  )
+            val relation = User.currentRentals >>: Rental.book
+            val result   = relation.startingFrom(user2, cache)
+
+            assertZIO(result)(equalTo(Chunk(book2, book3))) &&
+            assertZIO(proofs.calls.get)(
+              equalTo(
+                Calls(
+                  Book.fetch -> Chunk(book3.id)
                 )
               )
-            }
-          )
-        ),
-        suite("Unique datasource names")(
-          test("Two relations with the same name") {
-            proofs.flatMap { proofs =>
-              import proofs.*
-
-              val fetchUser = User.fetch
-              val user      = fetchUser.startingFromQuery(user1.id)
-
-              val fetchBook = Book.fetch
-              val book      = fetchBook.startingFromQuery(book1.id)
-
-              assertZIO((user <*> book).run)(equalTo((user1, book1)))
-            }
+            )
           }
-        )
+        }
+      ),
+      suite("Unique datasource names")(
+        test("Two relations with the same name") {
+          proofs.flatMap { proofs =>
+            import proofs.*
+
+            val fetchUser = User.fetch
+            val user      = fetchUser.startingFromQuery(user1.id)
+
+            val fetchBook = Book.fetch
+            val book      = fetchBook.startingFromQuery(book1.id)
+
+            assertZIO((user <*> book).run)(equalTo((user1, book1)))
+          }
+        }
       )
     )
 
