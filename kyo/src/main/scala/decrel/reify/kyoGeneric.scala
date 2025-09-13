4cc4ebcb9f4293058abaf163621ffb8bef22e78f
Scala Steward <scala_steward@virtuslab.com>
2025-09-13 23:54:05 +0000

    Update file headers with sbt-header 5.11.0
    Executed command: sbt headerCreate

--- kyo/src/main/scala/decrel/reify/kyoGeneric.scala
+++ kyo/src/main/scala/decrel/reify/kyoGeneric.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Haemin Yoo
+ * Copyright (c) 2022 Haemin Yoo
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
